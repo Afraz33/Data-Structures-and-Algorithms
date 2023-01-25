54aa1091ebd2a8554424c7198eb950a68fbe171e
Afraz33 <afraz3301@gmail.com>
2023-01-25 11:18:51 +0500

    First Commit

---
+++ Arrays/Missing number in array.js
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let arr = new Array(n-1);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n-1;i++)
+            arr[i] = input_ar1[i];
+        let obj = new Solution();
+        console.log(obj.MissingNumber(arr, n));
+        
+    }
+}// } Driver Code Ends
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} array
+ * @param {number} n
+ * @returns {number}
+*/
+
+// Formula used is n*(n+1)/2 - sum of array elements.
+class Solution{
+    MissingNumber(array,n){
+       var k, sum=0;
+       for(k=0;k<n-1;k++){
+           sum+= array[k];
+       }
+       var missingNumber = n*(n+1)/2 - sum;
+       return missingNumber;
+       
+    }
+}
