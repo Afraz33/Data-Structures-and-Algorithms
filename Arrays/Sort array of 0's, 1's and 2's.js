75104cab2b285c47812efea4650053141b07a335
Afraz33 <afraz3301@gmail.com>
2023-01-26 10:44:47 +0500

    2nd Problem

---
+++ Arrays/Sort array of 0's, 1's and 2's.js
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/* Function to print an array */
+function printArray(arr, size)
+{
+    let i;
+    let s='';
+    for (i=0; i < size; i++) {
+        s += arr[i] + " ";
+    }
+    console.log(s);
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let input_ar0 = readLine().split(' ').map(x=>parseInt(x));
+        let n = input_ar0[0];
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++)
+            arr[i] = input_ar1[i];
+        let obj = new Solution();
+        obj.sort012(arr, n);
+        printArray(arr, arr.length);
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} N
+ * @returns {number[]}
+*/
+
+class Solution {
+    
+     sort012(a, n){
+        let low = 0, high = n-1, mid = 0;
+        while(mid <= high){
+            if(a[mid]==0) {
+                let temp=a[low];
+                a[low]=a[mid];
+                a[mid]=temp;
+                low++;
+                mid++;
+            } else if(a[mid]==1) {
+                mid++;
+            } else {
+                let temp=a[mid];
+                a[mid]=a[high];
+                a[high]=temp;
+                high--;
+            }
+        }
+    }
+
+}
